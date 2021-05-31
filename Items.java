fd459dfb14f0747d378d9c73b2df3d3ff2518c30
Wajeeeeh <84520259+Wajeeeeh@users.noreply.github.com>
2021-05-31 14:27:01 -0400

    Add files via upload

---
+++ Items.java
@@ -0,0 +1,9 @@
+import java.awt.image.BufferedImage;
+
+abstract class Items extends GameObject{
+
+  Items(int x, int y, int width, int height, String name){
+    super(x, y, width,  height, name);
+  }
+
+}
