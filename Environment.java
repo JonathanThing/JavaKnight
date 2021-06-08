862a950cd2a87c2b8799849c0bdcef5a767801dd
SomeGuySomeGuy <76274095+SomeGuySomeGuy@users.noreply.github.com>
2021-06-08 10:59:46 -0400

    Add files via upload

---
+++ Environment.java
@@ -0,0 +1,12 @@
+import java.awt.Graphics;
+
+public abstract class Environment extends GameObject {
+
+	Environment(double x, double y, int width, int height, String name) {
+		super(x, y, width, height, name);
+
+	}
+
+	public abstract void draw(Graphics g);
+
+}
---
+++ HealthPack.java
@@ -0,0 +1,29 @@
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.util.ArrayList;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+class HealthPack extends Pickups {
+
+	private Rectangle healthPack;
+
+	HealthPack(int x, int y, int width, int height, String name) {
+		super(x, y, width, height, name);
+	}
+
+	public void drawItem(Graphics g) {
+		g.fillRect((int) getX() - getWidth()/2, (int) getY() - getHeight()/2, getWidth(), getHeight());
+	}
+
+	public boolean checkCollision(Player p) {
+		if (this.getCollision().intersects(p.getCollision())) {
+			return true;
+		}
+		
+		return false;
+
+	}
+
+}
