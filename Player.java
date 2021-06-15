d4f76d885ff98d9750605fed67339f991aa2a641
341042836 <84483261+341042836@users.noreply.github.com>
2021-06-14 23:11:11 -0400

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,6 +1,12 @@
+/**
+ * [Player.java]
+ * Description: The class for the player
+ * @author Jonathan, Ray, Wajeeh
+ * @version 1.0, May 28, 2021
+ */
+
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
-
 import java.io.File;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -11,179 +17,207 @@ import java.lang.Math;
 
 class Player extends Character {
 
-	// sprite
-	private BufferedImage sprite;
+ // sprite
+ private BufferedImage sprite;
 
-	// load sprite
-	public void loadSprite() {
-		try {
+ /**
+  * laserBeam
+  * method to draw the laserBeam
+  * @param g, the graphics object, x, the x coordinate of where the laser ends, y, 
+  * the y coordinate of where the laser ends, offSetX, how much the x is off by, offSetY, how much the y is off 
+  */ 
+ public void laserBeam(Graphics g, int x, int y, double offSetX, double offSetY) {
+   
+  g.setColor(Color.RED); //set color to red
+  g.drawLine((int) (this.getX() - offSetX), (int) (this.getY() - offSetY), (int) (x - offSetX),
+    (int) (y - offSetY)); //draws the laser beam in red
+  g.setColor(Color.BLACK); //set color back to black
+ }
 
-		} catch (Exception e) {
-			System.out.println("error loading sprite");
-		}
-		;
-	}
+ /**
+  * draw
+  * method to draw the player
+  * @param g, the graphics object, offSetX, how much the x is off by, offSetY, how much the y is off by
+  */
+ public void draw(Graphics g, double offSetX, double offSetY) {
+  g.drawImage(this.getSprite(), (int) (getX() - getWidth() / 2 - offSetX), (int) (getY() - getHeight() / 2 - offSetY), null);//draws the player
+ }
 
-	public void laserBeam(Graphics g, int x, int y, double offSetX, double offSetY) {
+ /**
+  * shoot
+  * method to allow the player to shoot
+  * @param targetX, the x coordinate of the target, targetY, the y coordinate of the tagret, sprite, the sprite for the projectile 
+  */
+ public void shoot(double targetX, double targetY, BufferedImage sprite) {
 
-		g.setColor(Color.RED);
-		g.drawLine((int) (this.getX() - offSetX), (int) (this.getY() - offSetY), (int) (x - offSetX),
-				(int) (y - offSetY));
-		g.setColor(Color.BLACK);
-	}
+  double xDifference = targetX - this.getX(); //calculate the difference in x
+  double yDifference = targetY - this.getY(); //calculate the difference in y
 
-	// draw
-	public void draw(Graphics g, double offSetX, double offSetY) {
+  double hyp = Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2)); //calculate hypotenuse to normalize the vector
 
-		g.drawImage(this.getSprite(), (int) (getX() - getWidth() / 2 - offSetX),
-				(int) (getY() - getHeight() / 2 - offSetY), null);
-	}
+  double xChange = ((xDifference / hyp) * 30); //calculate how much the x coord will change
+  double yChange = ((yDifference / hyp) * 30); //calculate how much the y coord will change
 
-	// create projectiles
-	public void shoot(double targetX, double targetY, BufferedImage sprite) {
+  if (this.getWeapon().getName().equals("shotgun")) { //if the player is using the shotgun
+   
+   for (int i = 0; i < 8; i++) {
+    this.getProjectilesList().add(new Projectile(getX(), getY(), this.getWeapon().getSize(),
+      this.getWeapon().getSize(), "Bullet",sprite, 20, xChange + Math.random() * (-16) + 8, yChange + Math.random() * (-16) + 8)); //add projectiles to arrayList
+   }
+  } else if (this.getWeapon().getName().equals("smg")) { //if the player is using the smg
+   this.getProjectilesList().add(new Projectile(getX(), getY(), this.getWeapon().getSize(), this.getWeapon().getSize(),
+     "Bullet",sprite, 20, xChange + Math.random() * (-2) + 1, yChange + Math.random() * (-4) + 1)); //add projectiles to arrayList
 
-		double xDifference = targetX - this.getX();
-		double yDifference = targetY - this.getY();
+  } else { //if the player is using the pistol
+   this.getProjectilesList().add(new Projectile(getX(), getY(), this.getWeapon().getSize(), this.getWeapon().getSize(),
+     "Bullet",sprite, 20, xChange, yChange)); //add projectiles to arrayList
+  }
+ }
 
-		double hyp = Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
+ /**
+  * moveProjectile
+  * method to move the projectiles
+  */
+ public void moveProjectile() {
 
-		double xChange = ((xDifference / hyp) * 30);
-		double yChange = ((yDifference / hyp) * 30);
+  for (int i = 0; i < this.getProjectilesList().size(); i++) { //loops through arrayList of projectiles
 
-		if (this.getWeapon().getName().equals("shotgun")) {
+   (getProjectilesList().get(i)).moveDown((getProjectilesList().get(i)).getChangeY()); //moves the projectils on the y-axis
+   (getProjectilesList().get(i)).moveRight((getProjectilesList().get(i)).getChangeX()); //moves the projectils on the x-axis
 
-			for (int i = 0; i < 8; i++) {
-				this.getProjectilesList()
-						.add(new Projectile(getX(), getY(), this.getWeapon().getSize(), this.getWeapon().getSize(),
-								"Bullet", sprite, 20, xChange + Math.random() * (-16) + 8,
-								yChange + Math.random() * (-16) + 8));
-			}
-		} else if (this.getWeapon().getName().equals("smg")) {
-			this.getProjectilesList()
-					.add(new Projectile(getX(), getY(), this.getWeapon().getSize(), this.getWeapon().getSize(),
-							"Bullet", sprite, 20, xChange + Math.random() * (-2) + 1,
-							yChange + Math.random() * (-4) + 1));
+  }
 
-		} else {
-			this.getProjectilesList().add(new Projectile(getX(), getY(), this.getWeapon().getSize(),
-					this.getWeapon().getSize(), "Bullet", sprite, 20, xChange, yChange));
-		}
-	}
+ }
 
-	// move projectiles
-	public void moveProjectile() {
+ /**
+  * drawPlayerProjectile
+  * method to draw the projectiles
+  * @param g, the graphics object, offSetX, how much the x is off by, offSetY, how much the y is off by
+  */
+ public void drawPlayerProjectile(Graphics g, double offSetX, double offSetY) {
+  for (int i = 0; i < getProjectilesList().size(); i++) { //loop through arrayList
+   (getProjectilesList().get(i)).draw(g, offSetX, offSetY); //draws the projectile
+  }
+ }
 
-		for (int i = 0; i < this.getProjectilesList().size(); i++) {
+ /**
+  * removeProjectile
+  * method to remove a certain projectile from the arrayList
+  * @param i, the index of the projectile to be removed
+  */
+ public void removeProjectile(int i) {
+  getProjectilesList().remove(i); //remove i from the arrayList
+ }
 
-			(getProjectilesList().get(i)).moveDown((getProjectilesList().get(i)).getChangeY());
-			(getProjectilesList().get(i)).moveRight((getProjectilesList().get(i)).getChangeX());
+ /**
+  * movement
+  * method to move the player
+  * @param up, the boolean for whether or nor the player decides to move up, down, the boolean for whether or nor the player decides to move down,
+  * left, the boolean for whether or nor the player decides to move left, right, the boolean for whether or nor the player decides to move right,
+  * list, the arrayList for enemies, map, the array of the map
+  */
+ public void movement(boolean up, boolean down, boolean left, boolean right, ArrayList<Enemy> list,
+   Environment[][] map) {
 
-		}
+  //variable for how much x and y change
+  double xMove = 0; 
+  double yMove = 0;
 
-	}
+  if (up) {
+   yMove += 1; //add 1 to the y change if up is true
+  }
 
-	// draw projectiles
-	public void drawPlayerProjectile(Graphics g, double offSetX, double offSetY) {
-		for (int i = 0; i < getProjectilesList().size(); i++) {
-			(getProjectilesList().get(i)).draw(g, offSetX, offSetY);
-		}
-	}
+  if (down) {
+   yMove -= 1; //subtract 1 to the y change if down is true
+  }
 
-	public void removeProjectile(int i) {
-		getProjectilesList().remove(i);
-	}
+  if (left) {
+   xMove -= 1; //subtract 1 to the x change if left is true
+  }
 
-	public void movement(boolean up, boolean down, boolean left, boolean right, ArrayList<Enemy> list,
-			Environment[][] map) {
+  if (right) {
+   xMove += 1; //add 1 to the x change if right is true
+  }
 
-		double xMove = 0;
-		double yMove = 0;
+  double hyp = Math.sqrt(Math.pow(xMove, 2) + Math.pow(yMove, 2)); //calculate how much the player changes diagonally 
 
-		if (up) {
-			yMove += 1;
-		}
+  if (hyp != 0) { //if the player moves
 
-		if (down) {
-			yMove -= 1;
-		}
+   this.moveRight((xMove / hyp) * 5); //move right
 
-		if (left) {
-			xMove -= 1;
-		}
+   if (collision(list, map)) { //if player hits the wall
 
-		if (right) {
-			xMove += 1;
-		}
+    this.moveLeft((xMove / hyp) * 5); //move back to original position
+   }
 
-		double hyp = Math.sqrt(Math.pow(xMove, 2) + Math.pow(yMove, 2));
+   this.moveUp((yMove / hyp) * 5); //move up
 
-		if (hyp != 0) {
+   if (collision(list, map)) { //if player hits the wall
 
-			this.moveRight((xMove / hyp) * 5);
+    this.moveDown((yMove / hyp) * 5); //move back to original position
 
-			if (collision(list, map)) {
+   }
 
-				this.moveLeft((xMove / hyp) * 5);
-			}
+  }
 
-			this.moveUp((yMove / hyp) * 5);
+ }
 
-			if (collision(list, map)) {
+  /**
+  * collision
+  * @param a, arrayList of enemies, b, array for map
+  * @return true if player collides with something, false if it does not
+  */
+ public boolean collision(ArrayList<Enemy> a, Environment[][] b) {
 
-				this.moveDown((yMove / hyp) * 5);
+  for (int i = 0; i < a.size(); i++) { //loop through enemy arrayList
+   if (this.getCollision().intersects(a.get(i).getCollision())) {
+    return true; //return true if player collides with an enemy
+   }
+  }
 
-			}
+  for (int i = 0; i < b.length; i++) { //loop through map
+   for (int j = 0; j < b[0].length; j++) {
+    if ((b[i][j] != null) && (!b[i][j].getName().equals("winblock")) && (this.getCollision().intersects(b[i][j].getCollision()))) {
+     return true; //return true if player collides with anything
+    }
+   }
+  }
+  return false; //return false if player doesn't collide with anything at all
+ }
 
-		}
+ /**
+  * wasHit
+  * @param a, the enemy that may hit the player
+  * @return true if player gets hit, false if it does not
+  */
+ public boolean wasHit(Enemy a) {
 
-	}
+  for (int i = 0; i < (a.getProjectilesList()).size(); i++) { //loop through projectiles of enemies 
 
-	public boolean collision(ArrayList<Enemy> a, Environment[][] b) {
+   if (a.getProjectilesList().get(i).getHitbox().intersects(this.getHitbox())) {
 
-		for (int i = 0; i < a.size(); i++) {
-			if (this.getCollision().intersects(a.get(i).getCollision())) {
-				return true;
-			}
-		}
+    a.getProjectilesList().remove(i); //remove the projectile that hits you if you get hit
 
-		for (int i = 0; i < b.length; i++) {
-			for (int j = 0; j < b[0].length; j++) {
-				if ((b[i][j] != null) && (!b[i][j].getName().equals("winblock"))
-						&& (this.getCollision().intersects(b[i][j].getCollision()))) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    return true; //return true if player gets hit
+   }
 
-	public boolean wasHit(Enemy a) {
+  }
 
-		for (int i = 0; i < (a.getProjectilesList()).size(); i++) {
+  return false; //return false if not hit
+ }
+ 
+ /**
+  * getAggro
+  * @return the range the player was to be within for enemies to aggro
+  */
+ public Rectangle getAggro() {
+  return new Rectangle((int)this.getX() - (32*40)/2, (int) this.getY() - (32*35)/2, 32*40, 32*35);
+ }
 
-			if (a.getProjectilesList().get(i).getHitbox().intersects(this.getHitbox())) {
+ // constructor
+ Player(double x, double y, int width, int height, String name, BufferedImage sprite, double health, Weapon weapon) {
+  super(x, y, width, height, name, sprite, health, weapon);
 
-				a.getProjectilesList().remove(i);
-
-				return true;
-			}
-
-		}
-
-		return false;
-	}
-
-	public Rectangle getAggro() {
-		return new Rectangle((int) this.getX() - (32 * 50) / 2, (int) this.getY() - (32 * 35) / 2, 32 * 50, 32 * 35);
-	}
-
-	// constructor
-	Player(double x, double y, int width, int height, String name, BufferedImage sprite, double health, Weapon weapon) {
-		// Player(int x, int y, int width, int height, BufferedImage sprite, String
-		// name, double health, String weapon, double ammo){
-		super(x, y, width, height, name, sprite, health, weapon);
-		// super(x, y, width,height, sprite, name, health, weapon);
-		loadSprite();
-	}
+ }
 
 }
--- Skeleton.java
+++ Skeleton.java
@@ -1,3 +1,9 @@
+/**
+ * [Skeleton.java]
+ * Description: The class for skeletons
+ * @author Jonathan, Ray, Wajeeh
+ * @version 1.0, June 5, 2021
+ */  
 
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
@@ -8,63 +14,82 @@ import java.util.ArrayList;
 import java.awt.Rectangle;
 
 class Skeleton extends Enemy {
-
-	private long lastShot;
-	private Player player;
-
-	public void shoot(Player player) {
-		int playerX = (int) (player.getX());
-		int playerY = (int) (player.getY());
-
-		double xDifference = getX() - player.getX();
-		double yDifference = getY() - player.getY();
-
-		double hyp = Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
-
-		double xChange = ((xDifference / hyp) * 7);
-		double yChange = ((yDifference / hyp) * 7);
-
-		if (this.getName().equals("eliteSkeleton")) {
-			for (int i = 0; i < 4; i++) {
-				this.getProjectilesList().add(new Projectile(getX(), getY(), 20, 20, "Bullet", getSprite(), 20,
-						xChange + Math.random() * -2 + 1, yChange + Math.random() * -2 + 1));
-			}
-		} else {
-			this.getProjectilesList()
-					.add(new Projectile(getX(), getY(), 20, 20, "Bullet", getSprite(), 20, xChange, yChange));
-
-		}
-	}
-
-	public void moveProjectile() {
-
-		for (int i = 0; i < this.getProjectilesList().size(); i++) {
-
-			(this.getProjectilesList().get(i)).moveUp((this.getProjectilesList().get(i)).getChangeY());
-			(this.getProjectilesList().get(i)).moveLeft((this.getProjectilesList().get(i)).getChangeX());
-
-		}
-
-	}
-
-	public void drawEnemyProjectile(Graphics g, double offSetX, double offSetY) {
-		for (int i = 0; i < this.getProjectilesList().size(); i++) {
-			(this.getProjectilesList().get(i)).draw(g, offSetX, offSetY);
-		}
-	}
-
-	Skeleton(double x, double y, int width, int height, String name, BufferedImage sprite, double health,
-			Weapon weapon) {
-		super(x, y, width, height, name, sprite, health, weapon);
-		lastShot = System.nanoTime();
-	}
-
-	public void attack(Player player) {
-
-		if (System.nanoTime() - lastShot >= 1e9 * this.getWeapon().getFireRate()) {
-			this.shoot(player);
-			lastShot = System.nanoTime();
-		}
-	}
-
+  
+  //variables
+  private long lastShot;
+  private Player player;
+  
+  /**
+   * shoot
+   * method to allow the skeleton to shoot
+   * @param player, the player, sprite, the sprite of the projectile 
+   */
+  public void shoot(Player player) {
+    int playerX = (int) (player.getX()); //get the x coord of the player
+    int playerY = (int) (player.getY()); //get the y coord of the player
+    
+    double xDifference = getX() - player.getX(); //calculate difference in x 
+    double yDifference = getY() - player.getY(); //calculate difference in y
+    
+    double hyp = Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2)); //calculate hypotenuse to help normalize the vector
+    
+    double xChange = ((xDifference / hyp)*7); //set the change in x for projectiles
+    double yChange = ((yDifference / hyp)*7); //set the change in y for projectiles
+    
+    
+    if (this.getName().equals("eliteSkeleton")) { //check if the skeleton is an elite skeleton (boss mob)
+      for (int i = 0 ; i < 4; i++) {
+        this.getProjectilesList().add(new Projectile(getX(), getY(), 20, 20, "Bullet", getSprite(), 20, xChange+Math.random()*-2 + 1, yChange+Math.random()*-2 + 1));
+      } //shoot 4 bullets
+    } else {
+      this.getProjectilesList().add(new Projectile(getX(), getY(), 20, 20, "Bullet", getSprite(), 20, xChange, yChange)); //only shoot 1 if the skeleton is normal
+      
+    }
+  }
+  
+  /**
+   * moveProjectile
+   * method that moves projectiles
+   */
+  public void moveProjectile() {
+    
+    for (int i = 0; i < this.getProjectilesList().size(); i++) { //loops through arrayList of projectiles
+      
+      (this.getProjectilesList().get(i)).moveUp((this.getProjectilesList().get(i)).getChangeY()); //moves the projectils on the y-axis
+      (this.getProjectilesList().get(i)).moveLeft((this.getProjectilesList().get(i)).getChangeX()); //moves the projectils on the x-axis
+      
+    }
+    
+  }
+  
+  /**
+   * drawPlayerProjectile
+   * method to draw the projectiles
+   * @param g, the graphics object, offSetX, how much the x is off by, offSetY, how much the y is off by
+   */
+  public void drawEnemyProjectile(Graphics g, double offSetX, double offSetY) {
+    for (int i = 0; i < this.getProjectilesList().size(); i++) { //loop through arrayList
+      (this.getProjectilesList().get(i)).draw(g, offSetX, offSetY); //draws the projectile
+    }
+  }
+  
+//constructor
+  Skeleton(double x, double y, int width, int height, String name, BufferedImage sprite, double health, Weapon weapon) {
+    super(x, y, width, height, name, sprite, health, weapon);
+    lastShot = System.nanoTime();
+  }
+  
+  /**
+   * shoot
+   * method to tells the skeleton when to shoot
+   * @param player, the player, sprite, the sprite of the projectile 
+   */
+  public void attack(Player player) {
+    
+    if (System.nanoTime()-lastShot >= 1e9 * this.getWeapon().getFireRate()) {
+      this.shoot(player); 
+      lastShot = System.nanoTime();
+    }
+  }
+  
 }
